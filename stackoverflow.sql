2c5714c908c93402a7d529eeae4bf9659ddc6f37
pd-sperrow <pdsperrow95@gmail.com>
2019-02-25 23:28:35 +0600

    update database

--- stackoverflow.sql
+++ stackoverflow.sql
@@ -15,9 +15,10 @@ CREATE TABLE `categories` (
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 INSERT INTO `categories` (`id`, `category_name`, `created_at`, `uddated_at`) VALUES
-(38,	'php',	NULL,	NULL),
 (39,	'laravel',	NULL,	NULL),
-(40,	'java',	NULL,	NULL);
+(40,	'java',	NULL,	NULL),
+(41,	'php',	NULL,	NULL),
+(42,	'',	NULL,	NULL);
 
 DROP TABLE IF EXISTS `comments`;
 CREATE TABLE `comments` (
@@ -26,7 +27,7 @@ CREATE TABLE `comments` (
   `post_id` int(10) unsigned NOT NULL,
   `comment` text CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL,
   `created_at` timestamp NULL DEFAULT NULL,
-  `update_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL,
   PRIMARY KEY (`id`),
   KEY `user_id` (`user_id`),
   KEY `post_id` (`post_id`),
@@ -34,8 +35,13 @@ CREATE TABLE `comments` (
   CONSTRAINT `comments_ibfk_2` FOREIGN KEY (`post_id`) REFERENCES `posts` (`id`) ON DELETE CASCADE
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
-INSERT INTO `comments` (`id`, `user_id`, `post_id`, `comment`, `created_at`, `update_at`) VALUES
-(1,	27,	1,	'gsgsgsggsgdg',	NULL,	NULL);
+INSERT INTO `comments` (`id`, `user_id`, `post_id`, `comment`, `created_at`, `updated_at`) VALUES
+(1,	27,	1,	'gsgsgsggsgdg',	'2019-02-16 17:28:26',	'2019-02-16 17:28:26'),
+(2,	26,	1,	'fsddgdsdg',	'2019-02-16 15:52:41',	'2019-02-16 15:52:41'),
+(4,	27,	1,	'fsdsgsgdds',	'2019-02-20 20:07:55',	'2019-02-20 20:07:55'),
+(7,	27,	13,	'safas',	'2019-02-20 20:35:20',	'2019-02-20 20:35:20'),
+(8,	27,	21,	'sdfgsdg',	'2019-02-25 06:39:40',	'2019-02-25 06:39:40'),
+(49,	27,	21,	'afasf',	'2019-02-25 11:34:23',	'2019-02-25 11:34:23');
 
 DROP TABLE IF EXISTS `posts`;
 CREATE TABLE `posts` (
@@ -54,12 +60,14 @@ CREATE TABLE `posts` (
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 INSERT INTO `posts` (`id`, `user_id`, `cat_id`, `title`, `body`, `created_at`, `updated_at`) VALUES
-(1,	27,	39,	'sgdsg',	'sdfgsdfgdfgsdfgsfgsgdsfsg',	NULL,	NULL),
-(2,	27,	38,	'AbracaDabra',	'fghdfhfg',	NULL,	NULL),
-(3,	27,	38,	'asdfsafs',	'asdfasfsadf',	NULL,	NULL),
-(6,	27,	39,	'final post',	'asfaflsdfl lhahsdfh l;lasdlfh loriaijdj',	NULL,	NULL),
-(7,	27,	38,	'AbracaDabra',	'hsthsrth',	NULL,	NULL),
-(8,	27,	39,	'fhcfh',	'gagadfga',	NULL,	NULL);
+(1,	27,	39,	'sgdsg',	'sdfgsdfgdfgsdfgsfgsgdsfsg',	'2019-02-16 16:33:07',	'2019-02-16 16:33:07'),
+(6,	27,	39,	'final post',	'asfaflsdfl lhahsdfh l;lasdlfh loriaijdj',	'2019-02-16 16:33:07',	'2019-02-16 16:33:07'),
+(8,	27,	39,	'fhcfh',	'gagadfga',	'2019-02-16 16:33:07',	'2019-02-16 16:33:07'),
+(9,	27,	40,	'post time check',	'this is for post time check',	'2019-02-16 16:33:07',	'2019-02-16 16:33:07'),
+(13,	27,	41,	'cbzx',	'asdfsadfsadfasfasdfasdf asdfasd asdf',	'2019-02-16 17:46:48',	'2019-02-16 17:46:48'),
+(14,	27,	39,	'accha kita kortham',	'dfgsdgsdfgsdfg sdfgs d',	'2019-02-16 17:46:27',	'2019-02-16 17:46:27'),
+(20,	27,	39,	'titlejla',	'asfasfasdfs it\'s okay bosss',	'2019-02-20 16:28:06',	'2019-02-20 16:28:06'),
+(21,	27,	40,	'AbracaDabraasfsadfas asdf',	'asfasdfasfsd asdfas sfsadcv',	'2019-02-20 16:28:19',	'2019-02-20 16:28:19');
 
 DROP TABLE IF EXISTS `replies`;
 CREATE TABLE `replies` (
@@ -79,6 +87,10 @@ CREATE TABLE `replies` (
   CONSTRAINT `replies_ibfk_5` FOREIGN KEY (`comment_id`) REFERENCES `comments` (`id`) ON DELETE CASCADE
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
+INSERT INTO `replies` (`id`, `post_id`, `comment_id`, `user_id`, `reply`, `created_at`, `updated_at`) VALUES
+(2,	1,	2,	27,	'afsaf',	'2019-02-25 11:17:36',	'2019-02-25 11:17:36'),
+(3,	21,	8,	27,	'sdfsg',	'2019-02-25 11:28:27',	'2019-02-25 11:28:27'),
+(4,	21,	8,	27,	'ssgsdfsg',	'2019-02-25 11:28:42',	'2019-02-25 11:28:42');
 
 DROP TABLE IF EXISTS `users`;
 CREATE TABLE `users` (
@@ -94,9 +106,9 @@ CREATE TABLE `users` (
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 INSERT INTO `users` (`id`, `username`, `email`, `password`, `bio`, `role`, `created_at`, `updated_at`) VALUES
-(24,	'hala',	'hala@mail.com',	'e10adc3949ba59abbe56e057f20f883e',	NULL,	0,	NULL,	NULL),
-(25,	'pias',	'pias@gmail.com',	'e10adc3949ba59abbe56e057f20f883e',	NULL,	0,	NULL,	NULL),
-(26,	'nikhil',	'nikhil@mail.com',	'e10adc3949ba59abbe56e057f20f883e',	NULL,	0,	NULL,	NULL),
-(27,	'sperrow',	'sperrow@gmail.com',	'e10adc3949ba59abbe56e057f20f883e',	NULL,	1,	NULL,	NULL);
+(24,	'hala',	'hala@mail.com',	'e10adc3949ba59abbe56e057f20f883e',	NULL,	0,	'2019-02-16 17:19:49',	'2019-02-16 17:19:49'),
+(25,	'pias',	'pias@gmail.com',	'e10adc3949ba59abbe56e057f20f883e',	NULL,	0,	'2019-02-16 17:19:49',	'2019-02-16 17:19:49'),
+(26,	'nikhil',	'nikhil@mail.com',	'e10adc3949ba59abbe56e057f20f883e',	NULL,	0,	'2019-02-16 17:19:49',	'2019-02-16 17:19:49'),
+(27,	'sperrow',	'sperrow@gmail.com',	'e10adc3949ba59abbe56e057f20f883e',	NULL,	1,	'2019-02-16 17:19:49',	'2019-02-16 17:19:49');
 
--- 2019-02-16 15:44:43
+-- 2019-02-25 17:27:39
