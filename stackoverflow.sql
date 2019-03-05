af37172372f482552dcd9e180b291c2d07eb12d2
pd-sperrow <pdsperrow95@gmail.com>
2019-03-05 13:09:48 +0600

    change sql file

--- stackoverflow.sql
+++ stackoverflow.sql
@@ -18,7 +18,7 @@ INSERT INTO `categories` (`id`, `category_name`, `created_at`, `uddated_at`) VAL
 (39,	'laravel',	NULL,	NULL),
 (40,	'java',	NULL,	NULL),
 (41,	'php',	NULL,	NULL),
-(42,	'',	NULL,	NULL);
+(42,	'javascript',	NULL,	NULL);
 
 DROP TABLE IF EXISTS `comments`;
 CREATE TABLE `comments` (
@@ -36,12 +36,11 @@ CREATE TABLE `comments` (
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 INSERT INTO `comments` (`id`, `user_id`, `post_id`, `comment`, `created_at`, `updated_at`) VALUES
-(1,	27,	1,	'gsgsgsggsgdg',	'2019-02-16 17:28:26',	'2019-02-16 17:28:26'),
-(2,	26,	1,	'fsddgdsdg',	'2019-02-16 15:52:41',	'2019-02-16 15:52:41'),
-(4,	27,	1,	'fsdsgsgdds',	'2019-02-20 20:07:55',	'2019-02-20 20:07:55'),
 (7,	27,	13,	'safas',	'2019-02-20 20:35:20',	'2019-02-20 20:35:20'),
 (8,	27,	21,	'sdfgsdg',	'2019-02-25 06:39:40',	'2019-02-25 06:39:40'),
-(49,	27,	21,	'afasf',	'2019-02-25 11:34:23',	'2019-02-25 11:34:23');
+(49,	27,	21,	'afasf',	'2019-02-25 11:34:23',	'2019-02-25 11:34:23'),
+(50,	27,	6,	'fgsdg',	'2019-03-04 16:52:09',	'2019-03-04 16:52:09'),
+(51,	27,	22,	'sdgdfgsdfg',	'2019-03-04 17:02:59',	'2019-03-04 17:02:59');
 
 DROP TABLE IF EXISTS `posts`;
 CREATE TABLE `posts` (
@@ -50,6 +49,7 @@ CREATE TABLE `posts` (
   `cat_id` int(10) unsigned NOT NULL,
   `title` varchar(60) CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL,
   `body` text CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL,
+  `approved` int(1) NOT NULL DEFAULT '0',
   `created_at` timestamp NULL DEFAULT NULL,
   `updated_at` timestamp NULL DEFAULT NULL,
   PRIMARY KEY (`id`),
@@ -59,15 +59,12 @@ CREATE TABLE `posts` (
   CONSTRAINT `posts_ibfk_2` FOREIGN KEY (`cat_id`) REFERENCES `categories` (`id`) ON DELETE CASCADE
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
-INSERT INTO `posts` (`id`, `user_id`, `cat_id`, `title`, `body`, `created_at`, `updated_at`) VALUES
-(1,	27,	39,	'sgdsg',	'sdfgsdfgdfgsdfgsfgsgdsfsg',	'2019-02-16 16:33:07',	'2019-02-16 16:33:07'),
-(6,	27,	39,	'final post',	'asfaflsdfl lhahsdfh l;lasdlfh loriaijdj',	'2019-02-16 16:33:07',	'2019-02-16 16:33:07'),
-(8,	27,	39,	'fhcfh',	'gagadfga',	'2019-02-16 16:33:07',	'2019-02-16 16:33:07'),
-(9,	27,	40,	'post time check',	'this is for post time check',	'2019-02-16 16:33:07',	'2019-02-16 16:33:07'),
-(13,	27,	41,	'cbzx',	'asdfsadfsadfasfasdfasdf asdfasd asdf',	'2019-02-16 17:46:48',	'2019-02-16 17:46:48'),
-(14,	27,	39,	'accha kita kortham',	'dfgsdgsdfgsdfg sdfgs d',	'2019-02-16 17:46:27',	'2019-02-16 17:46:27'),
-(20,	27,	39,	'titlejla',	'asfasfasdfs it\'s okay bosss',	'2019-02-20 16:28:06',	'2019-02-20 16:28:06'),
-(21,	27,	40,	'AbracaDabraasfsadfas asdf',	'asfasdfasfsd asdfas sfsadcv',	'2019-02-20 16:28:19',	'2019-02-20 16:28:19');
+INSERT INTO `posts` (`id`, `user_id`, `cat_id`, `title`, `body`, `approved`, `created_at`, `updated_at`) VALUES
+(6,	27,	39,	'final post',	'asfaflsdfl lhahsdfh l;lasdlfh loriaijdj',	1,	'2019-02-16 16:33:07',	'2019-02-16 16:33:07'),
+(13,	27,	41,	'cbzx',	'asdfsadfsadfasfasdfasdf asdfasd asdf',	1,	'2019-02-16 17:46:48',	'2019-02-16 17:46:48'),
+(20,	27,	39,	'titlejla',	'asfasfasdfs it\'s okay bosss',	1,	'2019-02-20 16:28:06',	'2019-02-20 16:28:06'),
+(21,	27,	40,	'AbracaDabraasfsadfas asdf',	'asfasdfasfsd asdfas sfsadcv',	1,	'2019-02-20 16:28:19',	'2019-02-20 16:28:19'),
+(22,	24,	42,	'sdgdfgssdfgsdfg',	'fgsdgsdfgsdgsdfgsdfgdg sdfgsdfgdf sdfgsd',	1,	'2019-03-04 17:02:12',	'2019-03-04 17:02:12');
 
 DROP TABLE IF EXISTS `replies`;
 CREATE TABLE `replies` (
@@ -88,9 +85,12 @@ CREATE TABLE `replies` (
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 INSERT INTO `replies` (`id`, `post_id`, `comment_id`, `user_id`, `reply`, `created_at`, `updated_at`) VALUES
-(2,	1,	2,	27,	'afsaf',	'2019-02-25 11:17:36',	'2019-02-25 11:17:36'),
 (3,	21,	8,	27,	'sdfsg',	'2019-02-25 11:28:27',	'2019-02-25 11:28:27'),
-(4,	21,	8,	27,	'ssgsdfsg',	'2019-02-25 11:28:42',	'2019-02-25 11:28:42');
+(4,	21,	8,	27,	'ssgsdfsg',	'2019-02-25 11:28:42',	'2019-02-25 11:28:42'),
+(5,	22,	51,	27,	'gsdgs',	'2019-03-04 17:38:21',	'2019-03-04 17:38:21'),
+(15,	22,	51,	27,	'sdfgs',	'2019-03-04 19:31:19',	'2019-03-04 19:31:19'),
+(16,	22,	51,	27,	'vghvghv',	'2019-03-04 19:43:06',	'2019-03-04 19:43:06'),
+(17,	22,	51,	27,	'asddfg',	'2019-03-04 19:43:38',	'2019-03-04 19:43:38');
 
 DROP TABLE IF EXISTS `users`;
 CREATE TABLE `users` (
@@ -111,4 +111,4 @@ INSERT INTO `users` (`id`, `username`, `email`, `password`, `bio`, `role`, `crea
 (26,	'nikhil',	'nikhil@mail.com',	'e10adc3949ba59abbe56e057f20f883e',	NULL,	0,	'2019-02-16 17:19:49',	'2019-02-16 17:19:49'),
 (27,	'sperrow',	'sperrow@gmail.com',	'e10adc3949ba59abbe56e057f20f883e',	NULL,	1,	'2019-02-16 17:19:49',	'2019-02-16 17:19:49');
 
--- 2019-02-25 17:27:39
+-- 2019-03-05 07:08:53
